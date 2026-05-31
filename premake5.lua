942e3b01d34ea5b686e10d1831bf20a2e2cc21e2
Samuel <samprogrammation@gmail.com>
2026-05-31 18:30:32 -0400

    add premake for build system

---
+++ premake5.lua
@@ -0,0 +1,34 @@
+project "imgui"
+    kind "StaticLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files {
+        "*.h",
+        "*.cpp",
+        "backends/imgui_impl_glfw.cpp",
+        "backends/imgui_impl_glfw.h",
+        "backends/imgui_impl_opengl3.cpp",
+        "backends/imgui_impl_opengl3.h"
+    }
+
+    includedirs {
+        "./",
+        "backends",
+        "../glfw/include"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        staticruntime "Off"
+
+    filter { "configurations:Debug" }
+        runtime "Debug"
+
+    filter { "configurations:Release" }
+        runtime "Release"
+
+    filter { "configurations:Dist" }
+        runtime "Release"
